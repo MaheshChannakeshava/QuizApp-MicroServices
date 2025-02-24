feac6c01b1b3427cc60c8fe0880d4d64f8a739ee
Mahesh Channakeshava <maheshchannakeshava7@gmail.com>
2025-02-24 23:34:58 +0000

    Add files via upload
    Added service registry using eureka server.

---
+++ service-registry/src/test/java/com/mahi/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahi.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
